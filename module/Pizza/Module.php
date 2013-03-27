7dc9107b9c4dda8ca3cc25efeb88ec8a56254453
Ralf Eggert <ralf@travello.com>
2013-03-27 18:21:47 +0100

    Projektdateien bereit gestellt

---
+++ module/Pizza/Module.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * ZF2 Buch Kapitel 5
+ * 
+ * @package    Pizza
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Alle Listings sind urheberrechtlich geschützt!
+ * @link       http://www.zendframeworkbuch.de/ und http://www.awl.de/2994
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Pizza;
+
+/**
+ * Application module class
+ * 
+ * @package    Pizza
+ */
+class Module
+{
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
